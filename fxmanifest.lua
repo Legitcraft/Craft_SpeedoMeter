ad63e0cb6a9e32ddaa9c74b41e913d2583ee1ef6
Legitcraft <142289837+Legitcraft@users.noreply.github.com>
2023-10-01 20:24:23 +0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Legitcraft'
+description 'Simple Speedometer for FiveM by Craft Resources'
+version 'Drawtext'
+
+client_scripts {
+    'client.lua'
+}
+
